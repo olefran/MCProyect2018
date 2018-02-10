012454abbbec14dcfd031cd30a06a48543ca2998
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2018-02-10 17:59:33 -0600

    Working final version

--- parte1/buscar.py
+++ parte1/buscar.py
@@ -8,34 +8,61 @@
 # What the fuck is this, someone please explain
 NUM_CHARS = 256
 
-def siguiente_estado():
-    pass
+def siguiente_estado(patron, pat_len, estado, caracter):
+    # Esta función obtiene el estado siguiente
+    # Si el caracter es igual al encontrado en el patrón
+    # incrementados el estado (nos movemos hacia adelante)
+    # sig_est guarda temporalmente el resultado.
+    i = 0 
+
+    if estado < pat_len and caracter == ord(patron[estado]):
+        return estado + 1
+    
+    for sig_est in range(estado, 0, -1): 
+        if ord(patron[sig_est - 1]) == caracter:
+            for i in range(sig_est - 1):   
+                if patron[i] != patron[estado - sig_est + 1 + i]:   
+                    break
+                i += 1
+            if i == (sig_est - 1):
+                return sig_est
+    return 0
 
 def crear_TF(patron, pat_len):
-    estado = 0
-    num_estado = 0
+    # Esta función construye la tabla de transiciones, 
+    # la cual representa el AFD para el patrón dado
+    global NUM_CHARS
+
+    # Inicialización de una tabla de transiciones de tamaño PAT_LEN X NUM_CHARS con valores vacíos
+    TF = [[0 for i in range(NUM_CHARS)] for i in range(pat_len + 1)]
 
     for estado in range(pat_len + 1):
-        for num_estado in range(NUM_CHARS):
-            TF[estado][num_estado] = siguiente_estado()
+        for caracter in range(NUM_CHARS):
+            TF[estado][caracter] = siguiente_estado(patron, pat_len, estado, caracter)
+
     return TF
 
 def buscar(patron, texto):
+    # Esta función simplemente imprime las ocurrencias del patrón
+    # en el texto y el lugar donde aparecen (contando desde 1)
+    global NUM_CHARS
     pat_len = len(patron)
     txt_len = len(texto)
     estado = 0
 
     # Creación de la tabla de transiciones
     TF = crear_TF(patron, pat_len)
+    # print(TF)
 
     # Procesa el texto utilizando la tabla de transiciones
+    
     for i in range(txt_len):
-        estado = TF[estado][texto[i]]
+        estado = TF[estado][ord(texto[i])]
         if (estado == pat_len):
-            print("\nPatrón encontrado en la locación: {}".format(i - pat_len + 1))
-
-# Función de entrada del programa, espera por el input y manda a llamar la función de búsqueda.    
+            print("Patrón encontrado en la locación: {}".format(i - pat_len + 2))
+    
 def main():
+    # Función de entrada del programa, espera por el input y manda a llamar la función de búsqueda.
     # Cadena sobre la que se buscará el patrón
     print("Introduce el texto sobre el que buscar el patrón")
     texto = input()
@@ -44,6 +71,7 @@ def main():
     print("Introduce un patrón a buscar")
     patron = input()
 
+    # Llamada a función de búsqueda
     buscar(patron, texto)
 
 if __name__ == '__main__':
