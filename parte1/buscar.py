cf456d60b1f84fa143463047f0e73c312b84acc7
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2018-02-10 14:54:10 -0600

    Main function

---
+++ parte1/buscar.py
@@ -0,0 +1,15 @@
+def main():
+    print("Introduce el texto sobre el que buscar el patrón")
+    texto = input()
+
+    print(texto)
+
+    print("Introduce un patron a buscar")
+    patron = input()
+
+    print(patron)
+
+    # buscar(patron, texto)
+
+if __name__ == '__main__':
+    main()
